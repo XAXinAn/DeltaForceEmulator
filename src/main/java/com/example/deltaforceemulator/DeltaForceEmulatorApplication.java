8e2d40971e77232c2c53025242a55566a59ab07c
Jiang Hangxi <jianghangximail@gmail.com>
2025-11-19 13:13:01 +0800

    初始文件

---
+++ src/main/java/com/example/deltaforceemulator/DeltaForceEmulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.deltaforceemulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeltaForceEmulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeltaForceEmulatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/deltaforceemulator/DeltaForceEmulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deltaforceemulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeltaForceEmulatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
